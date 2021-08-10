fda0155b69fb24e4ec1ae41c8e2d11f0cf6dfe4d
Minas <whiplashoo721@gmail.com>
2021-08-10 10:30:41 +0300

    add showInDialog conditional logic

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,74 +28,106 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _myGoogleFonts = [
+                                  "Abril Fatface",
+                                  "Aclonica",
+                                  "Alegreya Sans",
+                                  "Architects Daughter",
+                                  "Archivo",
+                                  "Archivo Narrow",
+                                  "Bebas Neue",
+                                  "Bitter",
+                                  "Bree Serif",
+                                  "Bungee",
+                                  "Cabin",
+                                  "Cairo",
+                                  "Coda",
+                                  "Comfortaa",
+                                  "Comic Neue",
+                                  "Cousine",
+                                  "Croissant One",
+                                  "Faster One",
+                                  "Forum",
+                                  "Great Vibes",
+                                  "Heebo",
+                                  "Inconsolata",
+                                  "Josefin Slab",
+                                  "Lato",
+                                  "Libre Baskerville",
+                                  "Lobster",
+                                  "Lora",
+                                  "Merriweather",
+                                  "Montserrat",
+                                  "Mukta",
+                                  "Nunito",
+                                  "Offside",
+                                  "Open Sans",
+                                  "Oswald",
+                                  "Overlock",
+                                  "Pacifico",
+                                  "Playfair Display",
+                                  "Poppins",
+                                  "Raleway",
+                                  "Roboto",
+                                  "Roboto Mono",
+                                  "Source Sans Pro",
+                                  "Space Mono",
+                                  "Spicy Rice",
+                                  "Squada One",
+                                  "Sue Ellen Francisco",
+                                  "Trade Winds",
+                                  "Ubuntu",
+                                  "Varela",
+                                  "Vollkorn",
+                                  "Work Sans",
+                                  "Zilla Slab"
+                                ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: FontPicker(
-            onFontChanged: (font) => {
-                  print(
-                      "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}")
-                },
-            pickerFont: 'Source Sans Pro',
-            googleFonts: [
-              "Abril Fatface",
-              "Aclonica",
-              "Alegreya Sans",
-              "Architects Daughter",
-              "Archivo",
-              "Archivo Narrow",
-              "Bebas Neue",
-              "Bitter",
-              "Bree Serif",
-              "Bungee",
-              "Cabin",
-              "Cairo",
-              "Coda",
-              "Comfortaa",
-              "Comic Neue",
-              "Cousine",
-              "Croissant One",
-              "Faster One",
-              "Forum",
-              "Great Vibes",
-              "Heebo",
-              "Inconsolata",
-              "Josefin Slab",
-              "Lato",
-              "Libre Baskerville",
-              "Lobster",
-              "Lora",
-              "Merriweather",
-              "Montserrat",
-              "Mukta",
-              "Nunito",
-              "Offside",
-              "Open Sans",
-              "Oswald",
-              "Overlock",
-              "Pacifico",
-              "Playfair Display",
-              "Poppins",
-              "Raleway",
-              "Roboto",
-              "Roboto Mono",
-              "Source Sans Pro",
-              "Space Mono",
-              "Spicy Rice",
-              "Squada One",
-              "Sue Ellen Francisco",
-              "Trade Winds",
-              "Ubuntu",
-              "Varela",
-              "Vollkorn",
-              "Work Sans",
-              "Zilla Slab"
-            ]),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            ElevatedButton(
+                child: Text('Pick a font (with a screen)'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => FontPicker(
+                                onFontChanged: (font) => {
+                                      print(
+                                          "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}")
+                                    },
+                                pickerFont: 'Source Sans Pro',
+                                googleFonts: _myGoogleFonts)),
+                  );
+                }),
+            ElevatedButton(
+                child: Text('Pick a font (with a dialog)'),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                          content: SingleChildScrollView(
+                        child: FontPicker(
+                                                          showInDialog: true,
+
+                            onFontChanged: (font) => {
+                                  print(
+                                      "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}")
+                                },
+                            pickerFont: 'Source Sans Pro',
+                            googleFonts: _myGoogleFonts),
+                      ));
+                    },
+                  );
+                }),
+          ],
+        )));
   }
 }
--- lib/flutter_font_picker.dart
+++ lib/flutter_font_picker.dart
@@ -1,27 +1,28 @@
 library flutter_font_picker;
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'constants/constants.dart';
+import 'font_picker_content.dart';
 
 //TODO: Basic fonts option (to be included in assets)
 //TODO: Filter languages according to selected fonts on init
-//TODO: Implement favorites feature
+//TODO: Implement favorites/recents feature
 
 class FontPicker extends StatefulWidget {
   final List<String> googleFonts;
-  final int recentCount;
   final ValueChanged<PickerFont> onFontChanged;
   final String pickerFont;
   final bool showFontInfo;
+  final bool showInDialog;
+  final int recentsCount;
 
   const FontPicker(
       {Key? key,
       this.googleFonts = GOOGLE_FONTS_LIST,
-      this.recentCount = 3,
       this.showFontInfo = true,
+      this.showInDialog = false,
+      this.recentsCount = 3,
       required this.onFontChanged,
       required this.pickerFont})
       : super(key: key);
@@ -31,310 +32,30 @@ class FontPicker extends StatefulWidget {
 }
 
 class _FontPickerState extends State<FontPicker> {
-  late List<PickerFont> _allFonts;
-  late List<PickerFont> _shownFonts;
-  late String _selectedFontFamily = "";
-  FontWeight _selectedFontWeight = FontWeight.w400;
-  FontStyle _selectedFontStyle = FontStyle.normal;
-  TextEditingController searchController = TextEditingController();
-  String _selectedFontLanguage = 'all';
-  List<String> _selectedFontCategories = List.from(GOOGLE_FONT_CATS);
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedFontFamily = widget.pickerFont;
-    _allFonts = widget.googleFonts
-        .map((fontFamily) => PickerFont(fontFamily: fontFamily))
-        .toList();
-    _shownFonts = _allFonts;
-  }
-
-  void changeFont(PickerFont selectedFont) {
-    setState(() {
-      widget.onFontChanged(selectedFont);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width / 2,
-              child: TextField(
-                controller: searchController,
-                decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.search),
-                    suffixIcon: IconButton(
-                      iconSize: 16.0,
-                      icon: const Icon(Icons.cancel),
-                      onPressed: () {
-                        FocusScope.of(context).unfocus();
-                        searchController.clear();
-                        onSearchTextChanged('');
-                      },
-                    ),
-                    hintText: 'Search...',
-                    border: InputBorder.none),
-                onChanged: onSearchTextChanged,
-              ),
-            ),
-            DropdownButton<String>(
-              value: _selectedFontLanguage,
-              icon: const Icon(Icons.arrow_drop_down_sharp),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedFontLanguage = newValue!;
-                  if (newValue == 'all') {
-                    _shownFonts = _allFonts;
-                  } else {
-                    _shownFonts = _allFonts
-                        .where((f) => f.subsets.contains(newValue))
-                        .toList();
-                  }
-                });
-              },
-              items: GOOGLE_FONT_LANGS.keys
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(GOOGLE_FONT_LANGS[value]!),
-                );
-              }).toList(),
-            )
-          ],
-        ),
-        Wrap(
-            children: GOOGLE_FONT_CATS.map((fontCategory) {
-          bool isSelectedCategory =
-              _selectedFontCategories.contains(fontCategory);
-          return SizedBox(
-            height: 30.0,
-            child: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                    backgroundColor: isSelectedCategory
-                        ? Theme.of(context).primaryColor
-                        : null,
-                    textStyle: TextStyle(
-                      fontSize: 10.0,
-                    ),
-                    shape: StadiumBorder()),
-                child: Text(fontCategory,
-                    style: TextStyle(
-                      color: isSelectedCategory ? Colors.white : null,
-                    )),
-                onPressed: () {
-                  setState(() {
-                    isSelectedCategory
-                        ? _selectedFontCategories.remove(fontCategory)
-                        : _selectedFontCategories.add(fontCategory);
-                    _shownFonts = _allFonts
-                        .where(
-                            (f) => _selectedFontCategories.contains(f.category))
-                        .toList();
-                  });
-                },
-              ),
-            ),
-          );
-        }).toList()),
-        SizedBox(
-            width: 300.0,
-            child: TextField(
-              style: GoogleFonts.getFont(_selectedFontFamily,
-                  fontWeight: _selectedFontWeight,
-                  fontStyle: _selectedFontStyle),
-              decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  hintText: 'The quick brown fox jumped over the lazy dog',
-                  hintStyle: TextStyle(
-                      fontStyle: _selectedFontStyle,
-                      fontWeight: _selectedFontWeight)),
-            )),
-        Expanded(
-            child: ListView.builder(
-          itemCount: _shownFonts.length,
-          itemBuilder: (context, index) {
-            var f = _shownFonts[index];
-            bool isBeingSelected = _selectedFontFamily == f.fontFamily;
-            String stylesString = widget.showFontInfo
-                ? f.variants.length > 1
-                    ? "  ${f.category}, ${f.variants.length} styles"
-                    : "  ${f.category}"
-                : "";
-            return ListTile(
-              selected: isBeingSelected,
-              selectedTileColor: Theme.of(context).focusColor,
-              onTap: () {
-                setState(() {
-                  _selectedFontFamily = f.fontFamily;
-                  _selectedFontWeight = FontWeight.w400;
-                  _selectedFontStyle = FontStyle.normal;
-                });
-              },
-              title: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: RichText(
-                    text: TextSpan(
-                        text: f.fontFamily,
-                        style: TextStyle(
-                            fontFamily:
-                                GoogleFonts.getFont(f.fontFamily).fontFamily,
-                            color: Colors.black),
-                        children: [
-                      TextSpan(
-                          text: stylesString,
-                          style: TextStyle(
-                              fontStyle: FontStyle.italic,
-                              fontSize: 11.0,
-                              color: Colors.grey,
-                              fontFamily: DefaultTextStyle.of(context)
-                                  .style
-                                  .fontFamily))
-                    ])),
-              ),
-              subtitle: isBeingSelected
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 4.0),
-                      child: Wrap(
-                          children: f.variants.map((variant) {
-                        bool isSelectedVariant;
-                        if (variant == "italic" &&
-                            _selectedFontStyle == FontStyle.italic) {
-                          isSelectedVariant = true;
-                        } else {
-                          isSelectedVariant =
-                              _selectedFontWeight.toString().contains(variant);
-                        }
-                        return SizedBox(
-                          height: 30.0,
-                          width: 60.0,
-                          child: Padding(
-                            padding: const EdgeInsets.all(2.0),
-                            child: OutlinedButton(
-                              style: OutlinedButton.styleFrom(
-                                  backgroundColor: isSelectedVariant
-                                      ? Theme.of(context).primaryColor
-                                      : null,
-                                  textStyle: TextStyle(
-                                    fontSize: 10.0,
-                                  ),
-                                  shape: StadiumBorder()),
-                              child: Text(
-                                variant,
-                                style: TextStyle(
-                                    fontStyle: variant == "italic"
-                                        ? FontStyle.italic
-                                        : FontStyle.normal,
-                                    color: isSelectedVariant
-                                        ? Colors.white
-                                        : null),
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  if (variant == "italic") {
-                                    _selectedFontStyle == FontStyle.italic
-                                        ? _selectedFontStyle = FontStyle.normal
-                                        : _selectedFontStyle = FontStyle.italic;
-                                  } else {
-                                    _selectedFontWeight =
-                                        FONT_WEIGHT_VALUES[variant]!;
-                                  }
-                                });
-                              },
-                            ),
-                          ),
-                        );
-                      }).toList()),
-                    )
-                  : null,
-              trailing: isBeingSelected
-                  ? TextButton(
-                      child: Text(
-                        'SELECT',
-                      ),
-                      onPressed: () {
-                        changeFont(PickerFont(
-                            fontFamily: _selectedFontFamily,
-                            fontWeight: _selectedFontWeight,
-                            fontStyle: _selectedFontStyle));
-                      },
-                    )
-                  : null,
-            );
-          },
-        ))
-      ],
-    );
-  }
-
-  void onSearchTextChanged(String text) {
-    if (text.isEmpty) {
-      setState(() {
-        _shownFonts = _allFonts;
-      });
-      return;
+    if (widget.showInDialog) {
+      return FontPickerContent(
+        onFontChanged: widget.onFontChanged,
+        googleFonts: widget.googleFonts,
+        showFontInfo: widget.showFontInfo,
+        showInDialog: widget.showInDialog,
+        recentsCount: widget.recentsCount,
+        pickerFont: widget.pickerFont,
+      );
     } else {
-      setState(() {
-        _shownFonts = _allFonts
-            .where(
-                (f) => f.fontFamily.toLowerCase().contains(text.toLowerCase()))
-            .toList();
-      });
+      return Scaffold(
+          appBar: AppBar(
+            title: Text("Pick a font:"),
+          ),
+          body: FontPickerContent(
+            onFontChanged: widget.onFontChanged,
+            googleFonts: widget.googleFonts,
+            showFontInfo: widget.showFontInfo,
+            showInDialog: widget.showInDialog,
+            recentsCount: widget.recentsCount,
+            pickerFont: widget.pickerFont,
+          ));
     }
-  }
-}
-
-class PickerFont {
-  final String fontFamily;
-  final FontWeight fontWeight;
-  final FontStyle fontStyle;
-  List<String> variants;
-  List<String> subsets;
-  String category;
-
-  PickerFont(
-      {required this.fontFamily,
-      this.fontWeight = FontWeight.w400,
-      this.fontStyle = FontStyle.normal})
-      : variants = parseVariants(fontFamily),
-        subsets = GOOGLE_FONTS[fontFamily]!["subsets"]!.split(","),
-        category = GOOGLE_FONTS[fontFamily]!["category"]!;
-
-  factory PickerFont.fromString(String fontSpec) {
-    final fontSpecSplit = fontSpec.split(":");
-    if (fontSpecSplit.length == 1) {
-      return PickerFont(fontFamily: fontSpecSplit[0]);
-    } else {
-      return PickerFont(
-          fontFamily: fontSpecSplit[0],
-          fontWeight:
-              FONT_WEIGHT_VALUES[fontSpecSplit[1].replaceAll("i", "")] ??
-                  FontWeight.w400,
-          fontStyle: fontSpecSplit[1].contains("i")
-              ? FontStyle.italic
-              : FontStyle.normal);
-    }
-  }
-
-  static List<String> parseVariants(String fontFamily) {
-    var variants = GOOGLE_FONTS[fontFamily]!["variants"]!.split(",");
-    if (variants.any((v) => v.contains("i"))) {
-      variants.add("italic");
-    }
-    variants.removeWhere((v) => v.endsWith("i"));
-    return variants;
-  }
-
-  String toFontSpec() {
-    String fontWeightString = this.fontWeight.toString();
-    String fontSpec =
-        "${this.fontFamily}:${fontWeightString.substring(fontWeightString.length - 3)}";
-    return this.fontStyle == FontStyle.italic ? "${fontSpec}i" : fontSpec;
   }
 }
