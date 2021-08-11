a191fbb8b2e2d8de6b1d48c6d8302481fc9af77d
Minas <whiplashoo721@gmail.com>
2021-08-11 20:17:26 +0300

    add paddings to example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_font_picker/flutter_font_picker.dart';
 
@@ -91,111 +92,118 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                child: Text('Pick a font (with a screen)'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => FontPicker(
-                            recentsCount: 10,
-                            onFontChanged: (font) {
-                              setState(() {
-                                _selectedFont = font.fontFamily;
-                                _selectedFontTextStyle = font.toTextStyle();
-                              });
-                              print(
-                                  "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}");
-                            },
-                            googleFonts: _myGoogleFonts)),
-                  );
-                }),
-            ElevatedButton(
-                child: Text('Pick a font (with a dialog)'),
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                          content: SingleChildScrollView(
-                        child: FontPicker(
-                            showInDialog: true,
-                            onFontChanged: (font) {
-                              setState(() {
-                                _selectedFont = font.fontFamily;
-                                _selectedFontTextStyle = font.toTextStyle();
-                              });
-                              print(
-                                  "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}");
-                            },
-                            googleFonts: _myGoogleFonts),
-                      ));
-                    },
-                  );
-                }),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('Pick a font: ',
-                      textAlign: TextAlign.right,
-                      style: TextStyle(fontWeight: FontWeight.w700)),
-                )),
-                Expanded(
-                  child: TextField(
-                    readOnly: true,
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                        suffixIcon: const Icon(Icons.arrow_drop_down_sharp),
-                        hintText: _selectedFont,
-                        border: InputBorder.none),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => FontPicker(
-                                onFontChanged: (font) {
-                                  setState(() {
-                                    _selectedFont = font.fontFamily;
-                                    _selectedFontTextStyle = font.toTextStyle();
-                                  });
-                                  print(
-                                      "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}");
-                                },
-                                googleFonts: _myGoogleFonts)),
-                      );
-                    },
+            child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  child: Text('Pick a font (with a screen)'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FontPicker(
+                              recentsCount: 10,
+                              onFontChanged: (font) {
+                                setState(() {
+                                  _selectedFont = font.fontFamily;
+                                  _selectedFontTextStyle = font.toTextStyle();
+                                });
+                                print(
+                                    "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}");
+                              },
+                              googleFonts: _myGoogleFonts)),
+                    );
+                  }),
+              ElevatedButton(
+                  child: Text('Pick a font (with a dialog)'),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                            content: SingleChildScrollView(
+                          child: FontPicker(
+                              showInDialog: true,
+                              onFontChanged: (font) {
+                                setState(() {
+                                  _selectedFont = font.fontFamily;
+                                  _selectedFontTextStyle = font.toTextStyle();
+                                });
+                                print(
+                                    "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}");
+                              },
+                              googleFonts: _myGoogleFonts),
+                        ));
+                      },
+                    );
+                  }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Pick a font: ',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(fontWeight: FontWeight.w700)),
+                  )),
+                  Expanded(
+                    child: TextField(
+                      readOnly: true,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                          suffixIcon: const Icon(Icons.arrow_drop_down_sharp),
+                          hintText: _selectedFont,
+                          border: InputBorder.none),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FontPicker(
+                                  onFontChanged: (font) {
+                                    setState(() {
+                                      _selectedFont = font.fontFamily;
+                                      _selectedFontTextStyle =
+                                          font.toTextStyle();
+                                    });
+                                    print(
+                                        "${font.fontFamily} with font weight ${font.fontWeight} and font style ${font.fontStyle}. FontSpec: ${font.toFontSpec()}");
+                                  },
+                                  googleFonts: _myGoogleFonts)),
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueGrey, width: 2.0)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Center(
-                        child: Column(children: [
-                      Text('Font: $_selectedFont',
-                          style: _selectedFontTextStyle),
-                      Text('The quick brown fox jumped',
-                          style: _selectedFontTextStyle),
-                      Text('over the lazy dog', style: _selectedFontTextStyle),
-                    ])),
-                  ),
-                ),
+                ],
               ),
-            )
-          ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blueGrey, width: 2.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Center(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                            Text('Font: $_selectedFont',
+                                style: _selectedFontTextStyle),
+                            Text('The quick brown fox jumped',
+                                style: _selectedFontTextStyle),
+                            Text('over the lazy dog',
+                                style: _selectedFontTextStyle),
+                          ])),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         )));
   }
 }
