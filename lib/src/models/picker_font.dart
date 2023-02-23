6284262e90017d2a23ce7dee50f5b23d1acd433f
Tim Maffett <timmaffett@gmail.com>
2023-02-23 11:15:30 -0800

    updates for generator capability

--- lib/src/models/picker_font.dart
+++ lib/src/models/picker_font.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../constants/constants.dart';
+import '../constants/fontweights_map.dart';
 
 /// A wrapper class that contains details about each font in the [FontPicker].
 ///
